606eb4f8ef4f1cacdc18aee0a096285935b66ca1
Nishant Nikam <nikamn@gmx.com>
2023-12-28 21:33:30 +0530

    28_12_evening

--- wpt/nodejs_assign/northwind/routes/router.js
+++ wpt/nodejs_assign/northwind/routes/router.js
@@ -2,14 +2,17 @@ const express = require("express");
 const router = express.Router();
 const conn = require("../db/dbconfig");
 
+// GET /
 router.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+// GET /customers
 router.get("/customers", (req, res) => {
   conn.query("SELECT * FROM customers", (err, results, fields) => {
     if (err) {
       console.log(`error occured during GET /customers query: `, err);
+      res.status(500).send("<h3>no data found</h3>");
     } else {
       //console.log(results);
       res.render("index", { customers: results });
@@ -17,10 +20,12 @@ router.get("/customers", (req, res) => {
   });
 });
 
+// GET /customers/add
 router.get("/customers/add", (req, res) => {
   res.render("add");
 });
 
+// POST /customers
 router.post("/customers", (req, res) => {
   let { CustomerName, ContactName, Address, City, PostalCode, Country } =
     req.body;
@@ -32,7 +37,7 @@ router.post("/customers", (req, res) => {
     [CustomerName, ContactName, Address, City, PostalCode, Country],
     (err, result, fields) => {
       if (err) {
-        console.log("err occurred in POST /customers/add : ", err);
+        console.log("err occurred in POST /customers : ", err);
         res.status(500).send("<h3>no data found</h3>");
       } else {
         res.redirect("/customers");
@@ -41,8 +46,42 @@ router.post("/customers", (req, res) => {
   );
 });
 
+// GET /customers/edit/:id
 router.get("/customers/edit/:id", (req, res) => {
+  connection.query(
+    "SELECT * FROM customers WHERE CustomerId=?",
+    [req.params.id],
+    function (err, result, fields) {
+      if (err) {
+        console.log(err);
+        res.status(500).send("<h3>no data found</h3>");
+      } else {
+        console.log(result);
+        res.status(200).send(result[0]);
+      }
+    }
+  );
+
   res.render("edit");
+});
+
+
+// DELETE /customers/delete/:id
+router.delete("/customers/delete/:id", (req, res) => {
+  let { id } = req.params;
+
+  conn.query(
+    "DELETE FROM customers WHERE CustomerId = ?",
+    id,
+    (err, result, fields) => {
+      if (err) {
+        console.log("err occurred in GET /customers/delete/:id : ", err);
+        res.status(500).send("<h3>no data found</h3>");
+      } else {
+        res.redirect("/customers");
+      }
+    }
+  );
 });
 
 module.exports = router;
