709a111f316e0d05043d3a7e722282818f3aa86f
Nishant D Nikam <nishant.nikam@proton.me>
2023-12-30 07:20:06 +0530

    wpt/react-express 30_12_morning_laptop

--- wpt/react_express/frontend_react/src/components/Footer.jsx
+++ wpt/react_express/frontend_react/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="bg-secondary navbar fixed-bottom">
-      <span className="m-auto">© 2024 Copyright : nikamn.in</span>
+      <span className="m-auto text-white">© 2024 Copyright : nikamn.in</span>
     </footer>
   );
 };
